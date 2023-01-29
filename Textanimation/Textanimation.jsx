647d41cd982f85254190b8a913939141973ce5f3
Alejandro Arra <97480427+alearra@users.noreply.github.com>
2023-01-29 04:12:14 -0300

    textanimation

---
+++ Textanimation/Textanimation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../Textanimation/Textanimation.css"
+
+export const Textanimation = () => {
+  return (
+    <section className="container">
+      <p>Hi, I´m Alejandro</p>
+      <div className="animation">
+        <div className="first">
+          <div>Full Stack Developer</div>
+        </div>
+        <div className="second">
+          <div>Project Manager</div>
+        </div>
+      </div>
+    </section>
+  );
+};
